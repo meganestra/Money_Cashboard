f25d54f4079ba8cdad1dff803e7c8fff074c98d1
Megan Strachan <m.e.strachan@gmail.com>
2016-07-03 11:57:35 +0100

    add initial tag model methods

--- models/tag.rb
+++ models/tag.rb
@@ -20,15 +20,40 @@ class Tag
   #transactions(show all associated transactions)
 
   #update(UPDATE)
+  # def self.update(options)
+  #   sql = "UPDATE tags SET description='#{options['description']}' WHERE id=#{options['id']}"
+  #   run(sql)
+  # end
 
   #show all(READ)
+  def self.show()
+    sql = "SELECT * FROM tags"
+    tags = Tag.map_items(sql)
+  end
 
   #find(one tag)
+  def self.find(id)
+    sql = "SELECT * FROM tags WHERE id = #{id}"
+    tag = Tag.map_item(sql)
+  end
 
   #delete all(DELETE)
   def self.delete_all()
     sql = "DELETE FROM tags"
     run(sql)
+  end
+
+  #map items
+  def self.map_items(sql)
+    tags = run(sql)
+    results = tags.map { |tag| Tag.new(tag) }
+    return results
+  end
+
+  #map item
+  def self.map_item(sql)
+    tag = Tag.map_items(sql)
+    return tag.first
   end
 
 end
