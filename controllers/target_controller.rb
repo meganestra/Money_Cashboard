f90da87927fefcca5baa1db39a110d686eae05b7
Megan Strachan <m.e.strachan@gmail.com>
2016-07-04 16:05:56 +0100

    add target controller and new erb

---
+++ controllers/target_controller.rb
@@ -0,0 +1,28 @@
+require('sinatra')
+require( 'sinatra/contrib/all' )
+require_relative('../models/account')
+require_relative('../models/tag')
+require_relative('../models/merchant')
+require_relative('../models/transaction')
+require_relative('../models/target')
+
+
+get '/target/new' do
+  erb(:'/target/new')
+end
+
+post '/target' do
+  @target = Target.new(params)
+  @target.save
+  erb(:'/target/create')
+end
+
+# get '/target' do
+#  @targets = Target.show()
+#  erb(:'/target/index')
+# end
+
+# get '/target/analysis' do
+#   @targets = Target.show()
+#   erb(:'target/target')
+# end
--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/transaction_controller')
 require_relative('controllers/account_controller')
+require_relative('controllers/target_controller')
 
 get '/' do
   erb (:home)
